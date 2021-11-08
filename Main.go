4f44c10c727773d5e6cef18c762a3a8e1145af17
patihomirov <patihomirov@gmail.com>
2021-11-09 00:21:51 +0300

    Подготовлен прием двух переменных в int

---
+++ Main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Учебная программа Калькулятор")
+	fmt.Println("Введите первое число")
+	var num1, num2 int
+	fmt.Scan(&num1)
+	fmt.Println("Вы ввели ", num1)
+	fmt.Println("Введите второе число")
+	fmt.Scan(&num2)
+	fmt.Println("Вы ввели ", num2)
+	/*
+		//	fmt.Println(a * 5)
+		for i := 0; i < a; i++ {
+			fmt.Println("ntcn", i)
+		}
+		//var x [5]int
+
+		x := make([]float64, 5)
+
+		x[4] = 100
+		x = append(x, 0.1)
+		fmt.Println(x)
+		fmt.Println(x[4] + 0.01)
+		//m, n, o := x[2:4]
+		//fmt.Println(m, n, o)
+		fmt.Println()
+	*/
+
+}
